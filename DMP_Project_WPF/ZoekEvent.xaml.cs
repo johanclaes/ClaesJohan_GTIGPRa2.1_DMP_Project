0c3e2acfc6afa6cfc49b275ab81aaa20731388d4
johanclaes <s0085147@student.thomasmore.be>
2022-11-27 22:06:13 +0100

    Add project files.

---
+++ DMP_Project_WPF/ZoekEvent.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DMP_Project_WPF
+{
+    /// <summary>
+    /// Interaction logic for ZoekEvent.xaml
+    /// </summary>
+    public partial class ZoekEvent : Window
+    {
+        public ZoekEvent()
+        {
+            InitializeComponent();
+        }
+    }
+}
